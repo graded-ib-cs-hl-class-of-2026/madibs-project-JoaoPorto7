b28d3fed175f17f7897368223a3b525619a51adb
porto <no>
2024-09-13 13:54:13 -0300

    Most of the way to a 5

--- src/App.java
+++ src/App.java
@@ -13,7 +13,61 @@ public class App {
         printGreeting(userName);
         printInstructions();
         doMadlib();
-        System.out.println("hi");
+    }
+
+    public String getUserName(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        return scanner.nextLine();
+    }
+
+    public void printGreeting(String userName) {
+        System.out.println("Hello, " + userName + "!");
+    }
+
+    public void printInstructions() {
+        System.out.println("In order to play this game you need to enter the required words into the console ");
+    }
+  
+    public void doMadlib(){
+
+
+        String verb;
+        String noun;
+        String adverb;
+        String pronoun;
+        String verb2;
+        String noun2;
+        String adverb2;
+        String pronoun2;
+        String ufcFighter;
+        String ufcFighter2;
+        Scanner input = new Scanner(System.in);
+   
+        System.out.println("Enter a verb");
+        verb = input.nextLine();
+        System.out.println("Enter a noun:");
+        noun = input.nextLine();
+        System.out.println("Enter a adverb:");
+        adverb = input.nextLine();
+        System.out.println("Enter a pronoun:");
+        pronoun = input.nextLine();
+
+        System.out.println("Enter a verb");
+        verb2 = input.nextLine();
+        System.out.println("Enter a noun:");
+        noun2 = input.nextLine();
+        System.out.println("Enter a adverb:");
+        adverb2 = input.nextLine();
+        System.out.println("Enter a pronoun:");
+        pronoun2 = input.nextLine();
+        System.out.println("Enter a UFC Fighter:");
+        ufcFighter = input.nextLine();
+        System.out.println("Enter another UFC Fighter:");
+        ufcFighter2 = input.nextLine();
+
+        System.out.println("One day, " + ufcFighter + " decided to " + verb + " with a " + noun + " " + adverb + ". While " + pronoun + " was doing this, " + ufcFighter2 + " came by and decided to " + verb2 + " over the " + noun2 + " " + adverb2 + ". After this, " + pronoun2 + " cheered for both fighters as they prepared for their next fight.");
+   
     }
     
     /* Here you should create all missing methods used by the start() method
@@ -21,9 +75,7 @@ public class App {
      * You can create any other methods you need as well!
      */ 
     
-    public void printInstructions() {
-        
-    }
+    
     
   
 
